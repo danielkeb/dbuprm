c18f946352ca0d827c0688b43f96699f9871dbcc
danielkeb <danielkebede381@gmail.com>
2024-08-29 08:31:50 -0400

    updated

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 const Config = {
-    ROOT_URL: process.env.REACT_APP_ROOT_URL || 'http://localhost:3333',
+    ROOT_URL: process.env.REACT_APP_ROOT_URL || 'https://dbuprm-backend.onrender.com',
     INSTANCE: process.env.REACT_APP_INSTANCE || 'axios.post'
   };
   
